c4eaf0ecce7354e41d6d6ccf830c243f449e4fa9
Agyei <hello@agyei.design>
2018-08-21 09:17:40 -0400

    upload?

---
+++ JAVASCRIPT KEARNING/game.js
@@ -0,0 +1,8 @@
+var secretNumber = 4;
+var guess=prompt("Guess a fucking number buddy");
+if(Number(guess) === secretNumber){
+  alert("ding ding bitch!");
+}
+else {
+  alert("wrong, motherfucker!");
+}
